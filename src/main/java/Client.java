e7153e119d87698d2dc5269bad1eb0cf6c472c01
ageekwall <agrawalsoumya94@gmail.com>
2020-03-13 20:05:09 -0500

    client-server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Client {
+    public static void main(String[] args) {
+        System.out.println("----******Welcome to the Bulletin Board******-----");
+
+    }
 }
